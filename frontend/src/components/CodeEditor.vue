0d786a3485382cda4a80ab6b9c0a8b3f60c6d8db
Steve <stev3npy@gmail.com>
2024-08-18 11:27:56 +0300

    fixed compatibility issues with vue-codemirror and replaced vue-codemirror with CodeMirror 6 for improved editor performance

--- frontend/src/components/CodeEditor.vue
+++ frontend/src/components/CodeEditor.vue
@@ -1,72 +1,102 @@
 <template>
-    <div class="code-editor">
-      <codemirror
-        v-model="code"
-        :options="cmOptions"
-        @input="onCodeChange"
-      />
-    </div>
-  </template>
-  
-  <script>
-  import { ref, computed, onMounted } from 'vue'
-  import { codemirror } from 'vue-codemirror'
-  import 'codemirror/lib/codemirror.css'
-  import 'codemirror/theme/monokai.css'
-  import 'codemirror/mode/javascript/javascript.js'
-  import 'codemirror/mode/python/python.js'
-  import 'codemirror/mode/go/go.js'
-  import 'codemirror/mode/css/css.js'
-  import 'codemirror/mode/htmlmixed/htmlmixed.js'
-  
-  export default {
-    name: 'CodeEditor',
-    components: {
-      codemirror
+  <div class="code-editor" ref="editorRef"></div>
+</template>
+
+<script>
+import { ref, onMounted, watch, onUnmounted } from 'vue'
+import { EditorState, basicSetup } from '@codemirror/basic-setup'
+import { EditorView, keymap } from '@codemirror/view'
+import { indentWithTab } from '@codemirror/commands'
+import { javascript } from '@codemirror/lang-javascript'
+import { python } from '@codemirror/lang-python'
+import { css } from '@codemirror/lang-css'
+import { html } from '@codemirror/lang-html'
+
+export default {
+  name: 'CodeEditor',
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
     },
-    props: {
-      initialCode: {
-        type: String,
-        default: ''
-      },
-      language: {
-        type: String,
-        default: 'javascript'
-      }
-    },
-    emits: ['update:code'],
-    setup(props, { emit }) {
-      const code = ref(props.initialCode)
-      
-      const cmOptions = computed(() => ({
-        tabSize: 2,
-        mode: props.language,
-        theme: 'monokai',
-        lineNumbers: true,
-        line: true,
-      }))
-  
-      const onCodeChange = (newCode) => {
-        code.value = newCode
-        emit('update:code', newCode)
-      }
-  
-      onMounted(() => {
-        // You can add any initialization logic here if needed
-      })
-  
-      return {
-        code,
-        cmOptions,
-        onCodeChange
+    language: {
+      type: String,
+      default: 'javascript'
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const editorRef = ref(null)
+    let view = null
+
+    const getLanguage = (lang) => {
+      switch (lang) {
+        case 'javascript': return javascript()
+        case 'python': return python()
+        case 'css': return css()
+        case 'html': return html()
+        default: return javascript()
       }
     }
+
+    const createEditor = () => {
+      const state = EditorState.create({
+        doc: props.modelValue,
+        extensions: [
+          basicSetup,
+          keymap.of([indentWithTab]),
+          getLanguage(props.language),
+          EditorView.updateListener.of((v) => {
+            if (v.docChanged) {
+              emit('update:modelValue', v.state.doc.toString())
+            }
+          })
+        ]
+      })
+
+      view = new EditorView({
+        state,
+        parent: editorRef.value
+      })
+    }
+
+    onMounted(() => {
+      createEditor()
+    })
+
+    watch(() => props.language, (newLang) => {
+      if (view) {
+        view.dispatch({
+          effects: EditorState.reconfigure.of([getLanguage(newLang)])
+        })
+      }
+    })
+
+    watch(() => props.modelValue, (newValue) => {
+      if (view && newValue !== view.state.doc.toString()) {
+        view.dispatch({
+          changes: { from: 0, to: view.state.doc.length, insert: newValue }
+        })
+      }
+    })
+
+    onUnmounted(() => {
+      if (view) {
+        view.destroy()
+      }
+    })
+
+    return {
+      editorRef
+    }
   }
-  </script>
-  
-  <style scoped>
-  .code-editor {
-    height: 100%;
-    width: 100%;
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+.code-editor {
+  height: 100%;
+  width: 100%;
+  border: 1px solid #ccc;
+}
+</style>
