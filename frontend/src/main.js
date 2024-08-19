a9b695e20191bf2f17d60ee9850daca0e76eb2b6
Steve <stev3npy@gmail.com>
2024-08-19 09:56:11 +0300

    updated to include fontawesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFolder, faFolderOpen, faFileAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFolder, faFolderOpen, faFileAlt)
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 store.dispatch('fetchUser').then(() => {
   if (store.getters.isAuthenticated) {
